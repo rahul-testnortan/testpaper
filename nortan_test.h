49d2e534a13e1718192196f7aced3bafd959127f
rahul-testnortan <72165721+rahul-testnortan@users.noreply.github.com>
2020-10-01 01:32:32 +0530

    Create nortan_test.h

---
+++ nortan_test.h
@@ -0,0 +1,11 @@
+#ifndef __NORTAN_TEST_H
+#define __NORTAN_TEST_H
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#endif /*  NORTAN_TEST_H */
